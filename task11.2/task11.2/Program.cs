2241a2b5ff0fe8f6b5b5821f106104028f22a61c
Solnikolf <QwQ@DESKTOP-K4V7M2X>
2025-12-17 12:50:21 +0500

    done

---
+++ task11.2/task11.2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Program
+{
+    
+    static void PrintArray(int[] array)
+    {
+        foreach (int item in array)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+
+    
+    static void MultiplyArray(int[] array, int k)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] *= k; 
+        }
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите целое положительное число n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n <= 0)
+        {
+            Console.WriteLine("Число должно быть положительным.");
+            return;
+        }
+
+        int[] numbers = new int[n];
+
+        
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Введите элемент {i + 1}: ");
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Исходный массив:");
+        PrintArray(numbers);
+
+        
+        Console.Write("Введите число k для умножения элементов массива: ");
+        int k = int.Parse(Console.ReadLine());
+
+        
+        MultiplyArray(numbers, k);
+
+        
+        Console.WriteLine($"Массив после умножения на {k}:");
+        PrintArray(numbers);
+    }
+}
---
+++ task11.2/task11.2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("task11.2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("task11.2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dec3ed1f-d315-4fc6-8570-381adf61f5c1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
