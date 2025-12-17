29e47f887cf800c5a46eb49e5049422f6483ca96
Solnikolf <QwQ@DESKTOP-K4V7M2X>
2025-12-17 12:48:15 +0500

    done

---
+++ task11.1/task11.1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    
+    static void PrintArray(int[] array)
+    {
+        foreach (int item in array)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine(); 
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите целое положительное число n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        
+        if (n <= 0)
+        {
+            Console.WriteLine("Число должно быть положительным.");
+            return;
+        }
+
+        int[] numbers = new int[n];
+
+        
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Введите элемент {i + 1}: ");
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        
+        Console.WriteLine("Массив, который вы ввели:");
+        PrintArray(numbers);
+    }
+}
